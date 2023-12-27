6105d8e1123592f271013946618e49e28eb915ba
quinnai9287 <keira0930@gmail.com>
2023-12-28 04:02:47 +0800

    feat: freeze

---
+++ _nuxt/icon.f1fcd21b.js
@@ -0,0 +1,1 @@
+import{M as e,an as a,ae as o}from"./entry.1c1b3320.js";import{d as c}from"./el-icon.0c316140.js";const r=c([String,Object,Function]),s={validating:e,success:a,error:o};export{s as V,r as i};
---
+++ _nuxt/useResize.252f60fc.js
@@ -0,0 +1,1 @@
+import{r as i,y as o,K as t}from"./entry.1c1b3320.js";function s(){const n=i("100vh"),e=()=>{n.value=`${window.innerHeight}px`};return o(()=>{window.addEventListener("resize",()=>e),e()}),t(()=>{window.removeEventListener("resize",()=>e)}),{dynamicHeight:n}}export{s as u};
