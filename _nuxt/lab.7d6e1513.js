110a7a29604e05a3626b7bb865a43de0847fb7d4
quinnai9287 <keira0930@gmail.com>
2024-07-22 07:22:08 +0800

    feat: ditto planet

--- _nuxt/lab.7d6e1513.js
+++
@@ -1,1 +0,0 @@
-import{N as a,r as o,o as t,c as s,P as n}from"./entry.1c1b3320.js";const m=a({__name:"lab",setup(r){return o("Some data to pass"),(e,l)=>(t(),s("main",null,[n(e.$slots,"default")]))}});export{m as default};
