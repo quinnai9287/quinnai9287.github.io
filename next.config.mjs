f530e0429ed4d59ef4d1d6d0525e77d7842fee0c
Stephani Aries <keira0930@gmail.com>
2025-02-23 22:01:14 +0800

    remove assetprefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.DEV ? '' : '/resume',
+  // assetPrefix: process.env.DEV ? '' : '/resume',
   reactStrictMode: true,
-  images: { unoptimized: true }
+  images: { unoptimized: true },
 };
-
 
 export default nextConfig;
